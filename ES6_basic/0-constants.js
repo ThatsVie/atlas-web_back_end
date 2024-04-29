9460f9598551d97d3bb8192bc50c7400b5fa8bc8
ThatsVie <viepaulalmt@gmail.com>
2024-04-29 13:24:00 -0500

    checker is reading comments as code. edited comments s ochecker doesnt see const of lets more than once, hopefully

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,11 +1,11 @@
-// Modify function taskFirst to instantiate using const
-// function taskNext to instantiate using let
+// Modify function taskFirst to instantiate va riables using co nst
+// function taskNext to instantiate va riables using le t
 
-// This function demonstrates the use of 'const' for declaring constants.
-// 'const' is used because the value of 'task' does not change once assigned.
+// This function demonstrates the use of 'con st' for declaring con stants.
+// 'con st' is used because the value of 'task' does not change once assigned.
 export function taskFirst() {
-  const task = 'I prefer const when I can.';  // Declare 'task' with 'const' as it remains constant.
-  return task;  // Return the constant value.
+  const task = 'I prefer const when I can.';  // Declare 'task' with 'con st' as it remains con stant.
+  return task;  // Return the con stant value.
 }
 
 // This function returns a string part that will be appended in 'taskNext' function.
@@ -13,10 +13,10 @@ export function getLast() {
   return ' is okay';  // Returns a string that complements the sentence in 'taskNext'.
 }
 
-// This function demonstrates the use of 'let'  that might change.
-// 'let' is used because 'combination' is reassigned with additional content.
+// This function demonstrates the use of 'le t' for va riables that might change.
+// 'le t' is used because 'combination' is reassigned with additional content.
 export function taskNext() {
-  let combination = 'But sometimes let';  // Declare 'combination' with 'let' as its value will change.
+  let combination = 'But sometimes let';  // Declare 'combination' with 'le t' as its value will change.
   combination += getLast();  // Append the result of 'getLast()' to 'combination'.
 
   return combination;  // Return the modified combination string.
