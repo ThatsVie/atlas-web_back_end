90d5b4e38bcf08064f4f49bae5a10447582bb0a0
ThatsVie <viepaulalmt@gmail.com>
2024-04-29 16:02:56 -0500

    Implement rest parameters in returnHowManyArguments

---
+++ ES6_basic/4-rest-parameter.js
@@ -0,0 +1,6 @@
+// Define the function with the rest parameter to accept any number of arguments.
+export default function returnHowManyArguments(...args) {
+  // 'args' variable represents an array containing all arguments passed to the function.
+  // Return the number of arguments by returning the length of the 'args' array.
+  return args.length;
+}
