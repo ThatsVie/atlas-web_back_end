b281fdc4a04f06a0c8f78da1db9a6d1a82e3b723
ThatsVie <viepaulalmt@gmail.com>
2024-04-30 17:13:46 -0500

    Add initializeRooms function to create array of ClassRoom instances

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,17 @@
+// Import the ClassRoom class from the file where it's defined
+import ClassRoom from './0-classroom';
+
+// Define the initializeRooms function which creates and returns an array of ClassRoom instances
+function initializeRooms() {
+  // Create an array of ClassRoom instances with specified sizes
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+    // Return the array of instances
+  return rooms;
+}
+
+// Export the initializeRooms function so it can be imported elsewhere
+export default initializeRooms;
