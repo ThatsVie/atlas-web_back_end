ebb6edfe00f62dcfdf8cc6f04a8d02d4106aa62b
ThatsVie <viepaulalmt@gmail.com>
2024-04-30 17:00:13 -0500

    Implement ClassRoom class with maxStudentsSize attribute

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+// Use 'export default' so this class can be imported in other files.
+export default class ClassRoom {
+  // Constructor method: Initializes a new instance of the ClassRoom class.
+  constructor(maxStudentsSize) {
+    // The constructor accepts maxStudentsSize as a parameter.
+    // This value is assigned to the _maxStudentsSize property of the class.
+    // The underscore (_) before maxStudentsSize indicates that it's a private property
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
