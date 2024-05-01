fe5ca2a43803aafa90a71d1157d171b7f919b346
ThatsVie <viepaulalmt@gmail.com>
2024-05-01 16:23:23 -0500

    Implement cloneCar method in Car class for type-preserving uninitialized clones

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,22 @@
+// Export the Car class as the default export of the module
+export default class Car {
+  // Constructor initializes a new Car instance with specified brand, motor, and color
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Static getter for Symbol.species defines constructor function used for creating new instances
+  static get [Symbol.species]() {
+    return this; // Returns the current class (Car or a subclass)
+  }
+
+  // Method to clone the car, creating a new instance of the class
+  cloneCar() {
+    // Retrieve the constructor function defined by Symbol.species
+    const Species = this.constructor[Symbol.species];
+    // Create a new instance of the species class with the same properties as the current instance
+    return new Species();
+  }
+}
