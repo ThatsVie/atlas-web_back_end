1ec3b347eb9cae296b5c59be14295bffb64d7c35
ThatsVie <viepaulalmt@gmail.com>
2024-05-02 12:29:34 -0500

    Create a function named getListStudents that returns an array of objects.

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+// Export the function using ES6 default export syntax
+export default function getListStudents() {
+  // Return an array of student objects directly from the function
+  return [
+    // Each object within the array represents a student
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
+}
