ba8826607efff2c7dd067bb7d60ba501c20d3934
ThatsVie <viepaulalmt@gmail.com>
2024-05-02 15:39:09 -0500

    Create a function named hasValuesFromArray that returns a boolean if all the elements in the array exist within the set.

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,7 @@
+export default function hasValuesFromArray(set, array) {
+  // Use the 'every' method on the array to test whether all elements pass the test
+  return array.every((value) => set.has(value));
+  // For each element in the array, check if the set contains this element
+  // The 'has' method of Set checks for the presence of an element and returns
+  // true if the element is found in the Set
+}
