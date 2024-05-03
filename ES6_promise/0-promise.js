f360f33b1aa1c4bc9c84eb15b4c8baf923bbd70b
ThatsVie <viepaulalmt@gmail.com>
2024-05-03 13:05:46 -0500

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  // Return a new Promise object without defining unused parameters
+  return new Promise(() => {
+    // No operation needed- just an unresolved promise
+  });
+}
