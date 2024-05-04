05e5667b9ea9bdec6cbffca48d4839dabeeb5e0c
ThatsVie <viepaulalmt@gmail.com>
2024-05-04 11:52:01 -0500

    comments

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 export default function signUpUser(firstName, lastName) {
   // return resolved Promise immediately
   return Promise.resolve({
-    firstName, // shorthand property firstName: firstName
-    lastName, // shorthand property lastName: lastName
+    firstName, // shorthand property
+    lastName, // shorthand property
   });
 }
