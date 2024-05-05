69807f0c66d6d0219a0734c81f17e96323238653
ThatsVie <viepaulalmt@gmail.com>
2024-05-05 12:51:47 -0500

    commenting

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,23 +1,23 @@
-// Import the getBudgetObject function from another module.
+// Import the getBudgetObject function
 import getBudgetObject from './7-getBudgetObject';
 
-// Define a function to extend basic budget object with additional methods for currency formatting.
+// function to extend basic budget object with additional methods 
 export default function getFullBudgetObject(income, gdp, capita) {
-  // Retrieve the basic budget object with specified income, gdp, and capita values.
+  // retrieve the basic budget object
   const budget = getBudgetObject(income, gdp, capita);
 
   // Extend the basic budget object with additional methods using ES6 method shorthand syntax.
   const fullBudget = {
-    ...budget, // Spread syntax to copy properties from the basic budget object into the fullBudget.
+    ...budget, // Spread syntax to copy properties from the basic budget object into fullBudget
 
-    // Define a method to format the income as a dollar value.
-    // The ES6 method shorthand is used here instead of the 'function' keyword.
+    // Method to format the income as a dollar value.
+    // The ES6 method shorthand is used here instead of the function keyword
     getIncomeInDollars(income) {
-      return `$${income}`; // Return the income prefixed with a dollar sign, indicating US currency.
+      return `$${income}`; // Return the income prefixed with a dollar sign for  US currency.
     },
 
-    // Define a method to format the income as a euro value.
-    // This also uses ES6 method shorthand for more concise function declaration.
+    // Method to format the income as a euro value.
+    // This also uses ES6 method shorthand
     getIncomeInEuros(income) {
       return `${income} euros`; // Return the income followed by the word "euros"
     },
