c8fad3d78f8531ee085a120cad84b8abe046f552
ThatsVie <viepaulalmt@gmail.com>
2024-05-05 12:39:38 -0500

    comments

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,23 +1,23 @@
 // Modify function taskFirst to instantiate va riables using co nst
 // function taskNext to instantiate va riables using le t
 
-// This function demonstrates the use of 'con st' for declaring con stants.
-// 'con st' is used because the value of 'task' does not change once assigned.
+// This function shows the use of con st for declaring con stants
+// con st is used because the value of task does not change once assigned
 export function taskFirst() {
-  const task = 'I prefer const when I can.'; // Declare 'task' with 'con st' as it remains con stant.
+  const task = 'I prefer const when I can.'; // Declare task with con st as it remains con stant
   return task; // Return the con stant value.
 }
 
-// This function returns a string part that will be appended in 'taskNext' function.
+// This function returns a string part that will be appended in taskNext function
 export function getLast() {
-  return ' is okay'; // Returns a string that complements the sentence in 'taskNext'.
+  return ' is okay'; // Returns a string that complements the sentence in taskNext
 }
 
-// This function demonstrates the use of 'le t' for va riables that might change.
-// 'le t' is used because 'combination' is reassigned with additional content.
+// shows the use of le t for va riables that might change.
+// le t is used because combination is reassigned with additional content.
 export function taskNext() {
-  let combination = 'But sometimes let'; // Declare 'combination' with 'le t' as its value will change.
-  combination += getLast(); // Append the result of 'getLast()' to 'combination'.
+  let combination = 'But sometimes let'; // declare combination with le t as its value will change.
+  combination += getLast(); // Append the result of getLast() to combination
 
   return combination; // Return the modified combination string.
 }
--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,17 +1,17 @@
-// Defines a constructor function called 'getNeighborhoodsList' for creating an object.
+// Defines a constructor function for creating an object
 export default function getNeighborhoodsList() {
-  // Initializes a property 'sanFranciscoNeighborhoods' on the object being created.
-  // This property is an array of string names representing neighborhoods in San Francisco.
+  // Initializes a property sanFranciscoNeighborhoods on the object being created
+  // This property is an array of string names 
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // Defines a method 'addNeighborhood' on the object using ES6 arrow syntax.
-  // Arrow functions capture the 'this' value of the function in which they are defined,
-  // which in this case is the 'getNeighborhoodsList' function.
-  // 'this' inside the arrow function refers to the object being created by 'getNeighborhoodsList'.
+  // Defines a method addNeighborhood on the object using ES6 arrow syntax.
+  // Arrow functions capture the this value of the function in which they are defined
+  // which in this case is the getNeighborhoodsList function.
+  // this inside the arrow function refers to the object being created by getNeighborhoodsList
   this.addNeighborhood = (newNeighborhood) => {
-    // Pushes a new neighborhood name to the 'sanFranciscoNeighborhoods' array.
-    // 'this' correctly refers to the object instance and its 'sanFranciscoNeighborhoods' property
-    // because of the lexical binding of 'this' in arrow functions.
+    // Pushes a new neighborhood name to the sanFranciscoNeighborhoods array.
+    // `this` correctly refers to the object instance and its sanFranciscoNeighborhoods property
+    // because of the lexical binding of this in arrow functions.
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
 
     // Returns the updated array of neighborhood names.
