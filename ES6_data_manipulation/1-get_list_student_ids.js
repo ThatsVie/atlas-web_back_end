021bff4b78290f5b8d2a854427d47fca73f0abda
ThatsVie <viepaulalmt@gmail.com>
2024-05-05 13:33:10 -0500

    comment

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,10 @@
-// Export the function using ES6 default export syntax
 export default function getListStudentIds(list) {
   // Check if the provided argument list is an array
   if (!Array.isArray(list)) {
-    // If list is not an array, return an empty array
+    // If list is not an array return an empty array
     return [];
   }
-  // If list is an array, use the map function to transform each student object in the array
+  // If list is an array use the map function to transform each student object in the array
   return list.map((student) => student.id);
   // The map function iterates over each element in the array, accessing the id property of
   // each student object and returns a new array containing only the ids
