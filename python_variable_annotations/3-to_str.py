9372da55813643ff058ccb37f0687734891897c4
ThatsVie <viepaulalmt@gmail.com>
2024-08-28 18:38:25 -0500

    Pycodestyle validation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''Defines a type-annotated function to return the string representation of a float.'''
+'''Defines a type-annotated function to return the
+string representation of a float.'''
 
 
 def to_str(n: float) -> str:
