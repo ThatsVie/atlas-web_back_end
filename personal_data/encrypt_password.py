9a12a115c1a326e10135f66e3693961600070123
ThatsVie <viepaulalmt@gmail.com>
2024-09-08 13:21:12 -0500

    Tarea 5

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,17 +10,8 @@ import bcrypt  # For password hashing
 def hash_password(password: str) -> bytes:
     '''
     Hashes a password using bcrypt with a salt.
-
-    Args:
-        password (str): The plain text password to be hashed.
-
-    Returns:
-        bytes: A salted, hashed password as a byte string.
     '''
-    # Generate a salt
     salt = bcrypt.gensalt()
-
-    # Hash the password using the generated salt
     hashed_password = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_password
